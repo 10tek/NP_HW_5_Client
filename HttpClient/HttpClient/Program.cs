cc696d9c569fbea67e4f52f21415c4d31a2b5675
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-01-30 18:34:34 +0600

    Клиент который отправляет HTTP запросы на сервер для авторизации и регистрации

---
+++ HttpClient/HttpClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var requestService = new RequestService();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Регистрация");
+                Console.WriteLine("2 - Авторизация");
+                Console.Write("Выбрать: ");
+                var choose = Console.ReadLine();
+                Console.Write("Login: ");
+                var log = Console.ReadLine();
+                Console.Write("Password: ");
+                var pas = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        Console.WriteLine(await requestService.SignUp(log, pas));
+                        break;
+                    case "2":
+                        Console.WriteLine(await requestService.SignIn(log,pas));
+                        break;
+                    default:
+                        Console.WriteLine("Am I joke to you?");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ HttpClient/HttpClient/RequestService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientApp
+{
+    public class RequestService
+    {
+        public async Task<string> SignUp(string login, string password)
+        {
+            var json = JsonConvert.SerializeObject(new User { Login = login, Password = password });
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "http://localhost/user/signup";
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, data);
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.Created:
+                        return "Аккаунт создан";
+                    case HttpStatusCode.Forbidden:
+                        return "Пользователь существует";
+                    case HttpStatusCode.NotFound:
+                        return "Пользователь не найден";
+                    default: return "Ошибка!";
+                }
+            }
+        }
+
+        public async Task<string> SignIn(string login, string password)
+        {
+            var json = JsonConvert.SerializeObject(new User { Login = login, Password = password });
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "http://localhost/user/auth";
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, data);
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                        return "Вы авторизированы!";
+                    case HttpStatusCode.Forbidden:
+                        return "Пользователь существует";
+                    case HttpStatusCode.NotFound:
+                        return "Пользователь с такими данными не найден!";
+                    default: return "Ошибка!";
+                }
+            }
+        }
+    }
+}
